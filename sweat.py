c29002cc626647aeec7a843cbb2218f4ba4e4ea7
bendgk <benkosten@gmail.com>
2017-11-19 10:01:46 -0500

    Rid of evidence

--- sweat.py
+++ sweat.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pyautogui
-import cv2
+import cv2, math, time
 
 from scipy import ndimage
 from scipy import misc
 
 from PIL import Image
+
+pyautogui.FAILSAFE = True
+center = (73, 75)
 
 def find_mob(radar):
     mob = (250, 60, 50)
@@ -41,10 +44,46 @@ def find_mob(radar):
 
     return mobs
 
+def closest_mob(mobs):
+    try:
+        d = []
+        for i in mobs:
+            d.append(math.sqrt((i[0] - center[0])**2 + (i[1] - center[1])**2))
+        return mobs[d.index(min(d))]
+    except:
+        print("No mobs in vicinity!")
+
+def mob_to_parallel(mob):
+    try:
+        mob_h = math.sqrt((mob[0] - center[0])**2 + (mob[1] - center[1])**2)
+        mob_x = mob[0] - center[0]
+        mob_y = mob[1] - center[1]
+
+        #if mob_y < 0: mob_h = -mob_h
+        
+        print(mob_x, mob_y, mob_h)
+        return math.asin(mob_x/mob_h)
+    except:
+        return 0
+
+def track(mob):
+    while True:
+        deg = mob_to_parallel(mob)
+        if deg < 0:
+            pyautogui.keyDown('c')
+            time.sleep(.1)
+            pyautogui.keyUp('c')
+        else:
+            pyautogui.keyDown('z')
+            time.sleep(.1)
+            pyautogui.keyUp('z')
+
 #while True:
 screen = pyautogui.screenshot('ss.png', region=(0, 80, 1024, 716))
 radar = screen.crop((876, 537, 1023, 684))
 radar.save('radar.png')
 mobs = find_mob(radar)
 print(mobs)
+print(mob_to_parallel(closest_mob(mobs))*180/math.pi)
 
+track(closest_mob(mobs))
