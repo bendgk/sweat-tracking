160a806553a694f1f7b1f86f723b4a05d646b20b
bendgk <benkosten@gmail.com>
2017-11-19 13:10:14 -0500

    Tracking now with walking

--- sweat.py
+++ sweat.py
@@ -10,6 +10,12 @@ from PIL import Image
 pyautogui.FAILSAFE = True
 center = (73, 75)
 
+#Keys
+z = 0x2C
+c = 0x2E
+w = 0x11
+target = 0x0B
+
 def find_mob(radar):
     mob = (250, 60, 50)
     mobs = []
@@ -60,44 +66,52 @@ def mob_to_parallel(mob):
         mob_y = mob[1] - center[1]
 
         #if mob_y < 0: mob_h = -mob_h
-        return math.asin(mob_x/mob_h) * 180/math.pi
+        return [math.asin(mob_x/mob_h) * 180/math.pi, mob_h]
     except:
         return 0
 
-def track():
-    z = 0x2C
-    c = 0x2E
+def track(distance_threshold, degree_threshold):
     key = None
     
-    while True:
+    screen = pyautogui.screenshot(region=(0, 80, 1024, 716))
+    radar = screen.crop((876, 537, 1023, 684))
+    mobs = find_mob(radar)
+    deg, distance = mob_to_parallel(closest_mob(mobs))
+    
+    while distance > distance_threshold and abs(deg) > degree_threshold:
         screen = pyautogui.screenshot(region=(0, 80, 1024, 716))
         radar = screen.crop((876, 537, 1023, 684))
     
         mobs = find_mob(radar)
-        deg = mob_to_parallel(closest_mob(mobs))
-        print(deg)
+        deg, distance = mob_to_parallel(closest_mob(mobs))
+        print(deg, distance)
         
-        
-        if deg > 10:
+        if deg > degree_threshold:
             if key != None:
                 directkeys.ReleaseKey(key)
             key = c
             directkeys.PressKey(c)
-        elif deg < -10:
+        elif deg < -degree_threshold:
             if key != None:
                 directkeys.ReleaseKey(key)
             key = z
             directkeys.PressKey(z)
-
         else:
             if key == None:
-                break
-            
-            directkeys.ReleaseKey(key)
-            break
+                pass
 
-        time.sleep(.05)
-                
-while True:
-    track()
+            else: directkeys.ReleaseKey(key)
+
+        directkeys.PressKey(w)
+        #time.sleep(.05)
+        
+    if key != None: directkeys.ReleaseKey(key)
+    directkeys.ReleaseKey(w)
+
+def main():
+    while True:
+        track(6, 10)
+        print("mob within vicinity!")
+        directkeys.PressKey(target)
+    
 
