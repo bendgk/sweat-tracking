b9571639eec7b7cfde41d9fa16da670d54f7d861
bendgk <benkosten@gmail.com>
2017-11-19 03:35:36 -0500

    Mobs just got tracked!

--- sweat.py
+++ sweat.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pyautogui
 import cv2
+
+from scipy import ndimage
+from scipy import misc
+
 from PIL import Image
 
 def find_mob(radar):
@@ -10,20 +14,37 @@ def find_mob(radar):
         b = []
         for x in range(147):
             if radar.getpixel((x, y)) == mob:
-                b.append((x, y))
+                b.append(1)
 
-            else: b.append(None)
+            else: b.append(0)
 
         mobs.append(b)
 
-    return mobs
-            
+    structure = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    mobs, mob_count = ndimage.measurements.label(mobs, structure=structure)
+
+    b = {}
+    for i in range(mob_count):
+        b[i] = list()
+
+    class BreakIt(Exception): pass
     
+    for y in range(len(mobs)):
+        for x in range(len(mobs)):
+            if mobs[y][x] != 0:
+                b[mobs[y][x] - 1].append((x, y))
+
+    mobs = []
+    for k in b.keys():
+        i = int(len(b[k])/2)
+        mobs.append(b[k][i])
+
+    return mobs
 
 #while True:
 screen = pyautogui.screenshot('ss.png', region=(0, 80, 1024, 716))
 radar = screen.crop((876, 537, 1023, 684))
 radar.save('radar.png')
 mobs = find_mob(radar)
-
+print(mobs)
 
